06117de31db0d775261fa8a085438c4fa473924d
Emil <suleymanovemil8@gmail.com>
2014-03-14 19:34:47 +0400

    JNI, zip, download

---
+++ ir/src/main/obj/local/armeabi/objs/jni_sonyopenir/libsonyir.o.d
@@ -0,0 +1,216 @@
+obj/local/armeabi/objs/jni_sonyopenir/libsonyir.o: jni/libsonyir.c \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/endian.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/termios.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/ioctl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/ioctl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/ioctl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/ioctls.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/termbits.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/termios.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/termios.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/time.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/poll.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/poll.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/poll.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/errno.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/memory.h \
+ jni/libsonyir.h jni/commands.h \
+ /home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/android/log.h
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/termios.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/ioctl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/ioctl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/ioctl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/ioctls.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/termbits.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/termios.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/termios.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/time.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/poll.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/poll.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/poll.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/memory.h:
+
+jni/libsonyir.h:
+
+jni/commands.h:
+
+/home/emil/development/android-ndk-r9/platforms/android-3/arch-arm/usr/include/android/log.h:
