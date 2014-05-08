5da43b2aea5977664e935216af387358fc9e2b46
Emil <suleymanovemil8@gmail.com>
2014-05-08 16:43:30 +0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := SonyIRRemote
+LOCAL_PACKAGE_NAME := IRRemote
 
 LOCAL_REQUIRED_MODULES := libjni_sonyopenir
 LOCAL_JNI_SHARED_LIBRARIES := libjni_sonyopenir
